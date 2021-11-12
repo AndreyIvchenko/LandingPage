9b11de5b7df1cb5d6d0cf4f652efc8614399c22a
AndreyIvchenko <6491666@gmail.com>
2021-11-12 12:45:00 +0300

    text-summaNum-consolClick

---
+++ scripts/landingPage.js
@@ -0,0 +1,17 @@
+function showString() {
+    let a = "text"
+    console.log(a)
+}
+showString()
+
+
+function summaNum(n1, n2) {
+    return n1 + n2;
+}
+console.log(summaNum(5, 5))
+
+document.getElementById('but1').addEventListener('click', butClick)
+
+function butClick() {
+    console.log("Click!")
+}
