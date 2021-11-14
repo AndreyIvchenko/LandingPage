26d27efe0bb3218d5cff981702ac2a1e6972c789
AndreyIvchenko <6491666@gmail.com>
2021-11-14 17:04:14 +0300

    popitka2_ppc_golova_krugom

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -1,17 +1,18 @@
-function showString() {
-    let a = "text"
-    console.log(a)
+function showCons(blabla) {
+    console.log('' + blabla)
 }
-showString()
-
+showCons(1)
 
 function summaNum(n1, n2) {
     return n1 + n2;
 }
-console.log(summaNum(5, 5))
+const res = summaNum(2.5, 2.5);
+console.log(res)
 
-document.getElementById('but1').addEventListener('click', butClick)
-
+let bid1 = document.getElementById("but1")
+console.log(bid1)
 function butClick() {
     console.log("Click!")
 }
+bid1.addEventListener('click', butClick)
+
